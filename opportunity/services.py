0e80e9d7e15a2c0e442a4a6baa23fe13da5487c4
Devin <devin.18@cse.mrt.ac.lk>
2021-09-29 17:32:21 +0530

    get opportunity added

--- opportunity/services.py
+++ opportunity/services.py
@@ -41,7 +41,6 @@ class OpportunityService:
     @staticmethod
     def getOpportunityById(opportunity_id):
         if Opportunity.objects.filter(id=opportunity_id, state=OpportunityState.APPROVED).exists():
-            return Opportunity.objects.get(id=opportunity_id).order_by(
-                "created_at").values()
+            return Opportunity.objects.get(id=opportunity_id).values()
 
         return None
