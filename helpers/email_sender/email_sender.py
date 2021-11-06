fa39df1fa997ba9631a646894208633c247a6b4f
Devin <devin.18@cse.mrt.ac.lk>
2021-11-07 02:01:12 +0530

    improving email functionality

--- helpers/email_sender/email_sender.py
+++ helpers/email_sender/email_sender.py
@@ -1,20 +1,27 @@
 import os
 
-from django.core.mail import EmailMultiAlternatives
+import sendgrid
 from dotenv import load_dotenv
+from sendgrid import Email, To, Content, Mail
 
 load_dotenv()
 
 
 def send_email(email, subject, text, html):
-    text_content = text
+    # text_content = text
     html_content = html
-    msg = EmailMultiAlternatives(subject, text_content, os.environ.get('COMPANY_EMAIL'), [email])
-    msg.attach_alternative(html_content, "text/html")
+    # msg = EmailMultiAlternatives(subject, text_content, os.environ.get('COMPANY_EMAIL'), [email])
+    # msg.attach_alternative(html_content, "text/html")
+    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email(os.environ.get('COMPANY_EMAIL'))
+    to_email = To(email)
+    subject = subject
+    content = Content("text/html", html_content)
+    mail = Mail(from_email, to_email, subject, content)
     try:
-        print(os.environ.get('COMPANY_EMAIL'))
-        print(email)
-        response = msg.send()
-        print(response)
+        response = sg.client.mail.send.post(request_body=mail.get())
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
     except Exception as e:
         print(e)
